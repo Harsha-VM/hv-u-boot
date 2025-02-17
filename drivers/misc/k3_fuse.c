6973f5360ee7cdea25a81a8862b505c290866538
Harsha Vardhan V M <h-vm@ti.com>
2025-02-17 19:26:15 +0530

    drivers: k3_fuse: Add fuse sub-system func calls
    Add K3_FUSE config option to add and enable fuse sub-system
    implementation function calls.
    
    Signed-off-by: Harsha Vardhan V M <h-vm@ti.com>

---
+++ drivers/misc/k3_fuse.c
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2025 Texas Instruments Incorporated, <www.ti.com>
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <fuse.h>
+#include <linux/arm-smccc.h>
+#include <string.h>
+
+#define K3_SIP_OTP_WRITEBUFF     0xC2000000
+#define K3_SIP_OTP_WRITE         0xC2000001
+#define K3_SIP_OTP_READ          0xC2000002
+
+int fuse_read(u32 bank, u32 word, u32 *val)
+{
+    struct arm_smccc_res res;
+
+    if (bank != 0U) {
+        printf("\nInvalid bank argument, ONLY bank 0 is supported\n");
+        return -EINVAL;
+    }
+
+    /* Make SiP SMC call and send the word in the parameter register */
+    arm_smccc_smc(K3_SIP_OTP_READ, word,
+            0, 0, 0, 0, 0, 0, &res);
+
+    *val = res.a1;
+    if (res.a0 != 0) {
+        printf("\nSMC call failed: Error code %ld\n", res.a0);
+        return -1; // Indicate failure
+    }
+    return res.a0;
+}
+
+int fuse_sense(u32 bank, u32 word, u32 *val)
+{
+    // TODO: fuse sense will be implemented in the second phase of driver udpate.
+    return -EPERM;
+}
+
+int fuse_prog(u32 bank, u32 word, u32 val)
+{
+    struct arm_smccc_res res;
+    u32 mask = val;
+
+    if (bank != 0U) {
+        printf("\nInvalid bank argument, ONLY bank 0 is supported\n");
+        return -EINVAL;
+    }
+
+    /* Make SiP SMC call and send the word, val and mask in the parameter register */
+    arm_smccc_smc(K3_SIP_OTP_WRITE, word,
+            val, mask, 0, 0, 0, 0, &res);
+
+    if (res.a0 != 0) {
+        printf("\nSMC call failed: Error code %ld\n", res.a0);
+        return -1; // Indicate failure
+    }
+    return res.a0;
+}
+
+int fuse_override(u32 bank, u32 word, u32 val)
+{
+    // TODO: fuse override will be implemented in the second phase of driver udpate.
+    return -EPERM;
+}
+
+int fuse_writebuff(ulong addr)
+{
+    struct arm_smccc_res res;
+
+    /* Make SiP SMC call and send the addr in the parameter register */
+    arm_smccc_smc(K3_SIP_OTP_WRITEBUFF, (unsigned long)addr,
+            0, 0, 0, 0, 0, 0, &res);
+
+    if (res.a0 != 0) {
+        printf("\nSMC call failed: Error code %ld\n", res.a0);
+        return -1; // Indicate failure
+    }
+    return res.a0;
+}
